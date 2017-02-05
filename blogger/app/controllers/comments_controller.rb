a1bc8cb875f4768ab61915db3a7cc601d07ad042
MarshallM1 <tebag9084@gmail.com>
2017-02-05 14:45:41 -0800

    Added tagging feature

--- blogger/app/controllers/comments_controller.rb
+++ blogger/app/controllers/comments_controller.rb
@@ -8,7 +8,9 @@ class CommentsController < ApplicationController
 		redirect_to article_path(@comment.article)
 	end
 
-	def comment_params
-  		params.require(:comment).permit(:author_name, :body)
-	end
+	private
+
+		def comment_params
+	  		params.require(:comment).permit(:author_name, :body)
+		end
 end
